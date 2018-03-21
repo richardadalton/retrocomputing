8fd531acd936ddbdf23452954eff3ce346362732
Richard Dalton <richard@codeinstitute.net>
2018-03-21 09:49:09 +0000

    Can Add To Cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,18 +14,20 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from home.views import home
+from products.views import all_products
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from cart import urls as urls_cart
 from django.views import static
 from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^$', all_products, name='home'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
