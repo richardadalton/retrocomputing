071a4de2c2ba9675476a001ace9b374355d5fd5d
Richard Dalton <richard@codeinstitute.net>
2018-03-21 12:24:21 +0000

    Fixed Totals in Cart

--- cart/views.py
+++ cart/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
-
+from decimal import Decimal
 
 def view_cart(request):
     
     cart = request.session.get('cart', {})
 
     cart_items = []
+    total = 0
     for item_id, item_quantity in cart.items():
         this_product = get_object_or_404(Product, pk=item_id)
+        this_total = this_product.price * Decimal(item_quantity)
+        total += this_total
         this_item = {
             'image': this_product.image,
             'name': this_product.name,
             'quantity': item_quantity,
             'price': this_product.price,
-            'total': 0,
+            'total': this_total,
         }
         cart_items.append(this_item)
     
-    args = {'cart_items': cart_items, 'total': 999 }
+    args = {'cart_items': cart_items, 'total': total }
     
     return render(request, "cart/view_cart.html", args)
     
