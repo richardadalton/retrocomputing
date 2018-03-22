a65c28fe54047a4ac31d7a5c22a8c3962cc76a14
Richard Dalton <richard@codeinstitute.net>
2018-03-22 18:41:01 +0000

    Can view ratings and avg ratings as stars

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg
 
 # Create your models here.
 class Product(models.Model):
@@ -6,6 +7,15 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+
+    @property
+    def average_rating(self):
+        average = self.reviews_received.all().aggregate(Avg('rating'))
+        return int(average['rating__avg'])
+
+    @property
+    def stars(self):
+        return range(self.average_rating)
 
     def __str__(self):
         return self.name
