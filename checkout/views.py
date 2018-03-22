acbdd69f15e202c106776238de2cc2487d9c7a62
Richard Dalton <richard@codeinstitute.net>
2018-03-22 16:34:05 +0000

    Stripe Publishable Key set in checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.conf import settings
 from .forms import OrderForm, MakePaymentForm
 from products.models import Product
 from decimal import Decimal
@@ -37,7 +38,7 @@ def checkout(request):
     else:
         order_form = OrderForm()
         payment_form = MakePaymentForm()
-        context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
+        context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE_KEY }
         cart = request.session.get('cart', {})
         cart_items_and_total = get_cart_items_and_total(cart)
         context.update(cart_items_and_total)
