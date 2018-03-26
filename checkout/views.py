71ba9b3f20efb0a90038797b7021ca5f13211074
Richard Dalton <richard@codeinstitute.net>
2018-03-26 11:43:35 +0000

    Refactor checkout view a bit

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.conf import settings
 from .forms import OrderForm, MakePaymentForm
 from products.models import Product
 from decimal import Decimal
 from cart.utils import get_cart_items_and_total
 from django.utils import timezone
 from .models import OrderLineItem
-import stripe
 from django.contrib import messages
 from cart.utils import get_cart_items_and_total
-from django.core.mail import send_mail
-from django.template.loader import get_template, render_to_string
-
-stripe.api_key = settings.STRIPE_SECRET
+from .utils import save_order_items, charge_card, send_confirmation_email
+import stripe
+from django.conf import settings
 
 # Create your views here.
 def checkout(request):
@@ -29,49 +26,23 @@ def checkout(request):
         
             # Save the Order Line Items
             cart = request.session.get('cart', {})
-            for id, quantity in cart.items():
-                product = get_object_or_404(Product, pk=id)
-                order_line_item = OrderLineItem(
-                    order = order,
-                    product = product,
-                    quantity = quantity
-                    )
-                order_line_item.save()
-        
+            save_order_items(order, cart)
         
             # Charge the Card
             items_and_total = get_cart_items_and_total(cart)
             total = items_and_total['total']
-            total_in_cent = int(total*100)
+            stripe_token=payment_form.cleaned_data['stripe_id']
+
             try:
-                customer = stripe.Charge.create(
-                    amount=total_in_cent,
-                    currency="EUR",
-                    description="Dummy Transaction",
-                    card=payment_form.cleaned_data['stripe_id'],
-                )
+                customer = charge_card(stripe_token, total)
             except stripe.error.CardError:
                 messages.error(request, "Your card was declined!")
-
 
             if customer.paid:
                 messages.error(request, "You have successfully paid")
 
                 # Send Email
-                context = {
-                    'site_name': "Blah Blah dot com",
-                    'user': request.user,
-                }
-                context.update(items_and_total)
-                message = render_to_string('checkout/text_confirmation_email.html', context)
-                html_message = render_to_string('checkout/html_confirmation_email.html', context)
-                
-                subject = 'Thanks for buying our stuff!'
-                message = message
-                from_email = settings.SYSTEM_EMAIL
-                to_email = [request.user.email]
-    
-                send_mail(subject,message,from_email,to_email,fail_silently=True,html_message=html_message)
+                send_confirmation_email(request.user.email, request.user, items_and_total)
         
                 #Clear the Cart
                 del request.session['cart']
